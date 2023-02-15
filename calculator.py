5cc59153e0f79fb4e293f98f1256a350baa47648
brianmungai <briannancy911@gmail.com>
2023-02-15 00:37:59 -0800

    wednesday

--- calculator.py
+++ calculator.py
@@ -4,13 +4,25 @@ number2 = eval(input("enter 2nd number"))
 operator = input ("enter operator")
 def add (num1, num2):
     result = num1 + num2
-    return result
+    print (result)
 def subtract (num1, num2):
     result = num1 - num2
-    return result
-def divide (num1 / num2):
-    result num1 / num2
-    return result
-def multiply (num1 * num2):
-    result num1 * num2
-    return result
+    print (result)
+def divide (num1 , num2):
+    result = num1 / num2
+    print (result)
+def multiply (num1,num2):
+    result = num1 * num2
+    print (result)
+if operator == "+":
+    add(number1,number2)
+elif operator == "-":
+    subtract(number1,number2)
+elif operator == "/":
+    divide(number1,number2)
+elif operator == "X" or "x" or "*":
+    multiply(number1,number2)
+else:
+    print("invalid operator")
+
+
